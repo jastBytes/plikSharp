dbd385f5b2e9bd5618d8464750dfb588222ea13f
Jan Steffen <jan.steffen@advision-digital.de>
2018-03-06 09:49:04 +0100

    Refactor to bundle as single project and nuget package

---
+++ iss0.Plik.Client/Models/UploadFile.cs
@@ -0,0 +1,15 @@
+﻿namespace iss0.Plik.Models
+{
+    public class UploadFile
+    {
+        public string Id { get; set; }
+        public string FileName { get; set; }
+        public string FileMd5 { get; set; }
+        public string Status { get; set; }
+        public string FileType { get; set; }
+        public int FileUploadDate { get; set; }
+        public int FileSize { get; set; }
+        public string Reference { get; set; }
+        public string DownloadUrl(string uploadId) => $"/file/{uploadId}/{Id}/{FileName}";
+    }
+}
